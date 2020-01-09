c6c1a233d3a287f770a96e2664ae7d1de5415191
ahmad4hat <rappar.stand@gmail.com>
2020-01-09 21:32:45 +0600

    models added for products

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+# import product to add to the admin panel
+from .models import Product
+
+admin.site.register(Product)
 
 # Register your models here.
--- products/models.py
+++ products/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=500)
+    urls = models.TextField()
+    body = models.TextField()
+    pub_date = models.DateTimeField()
+    votes_total = models.IntegerField(default=1)
+    image = models.ImageField(upload_to="images/")
+    icon = models.ImageField(upload_to="images/")
+    hunter = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):  # this chages the name what is seen from the admin panel
+        return self.title
+
+    # return a short body
+    def summery(self):
+        return self.body[:100]
+
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
